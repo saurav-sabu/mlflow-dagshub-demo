7ddde527a943b4eb36497b318245abff8d6b1032
Saurav Sabu <59791440+saurav-sabu@users.noreply.github.com>
2024-11-11 20:42:19 +0000

    added random forest exp

--- iris_rf.py
+++ iris_rf.py
@@ -3,7 +3,7 @@ import mlflow.sklearn
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 import matplotlib.pyplot as plt
@@ -21,15 +21,16 @@ y = iris.target
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-max_depth = 10
+max_depth = 1
+n_estimators = 100
 
 mlflow.set_experiment("iris-dt")
 
 with mlflow.start_run():
-    dt = DecisionTreeClassifier(max_depth=max_depth)
-    dt.fit(X_train,y_train)
+    rf = RandomForestClassifier(max_depth=max_depth,n_estimators=n_estimators)
+    rf.fit(X_train,y_train)
 
-    y_pred = dt.predict(X_test)
+    y_pred = rf.predict(X_test)
 
     acc = accuracy_score(y_test,y_pred)
 
@@ -44,7 +45,8 @@ with mlflow.start_run():
     mlflow.log_artifact("confusion_matrix.png")
     mlflow.log_metric("accuracy",acc)
     mlflow.log_param("max_depth",max_depth)
+    mlflow.log_param("n estimators",n_estimators)
     mlflow.log_artifact(__file__)
-    mlflow.sklearn.log_model(dt,"Decision Tree")
+    mlflow.sklearn.log_model(rf,"Random Forest")
     mlflow.set_tag("author","saurav")
-    mlflow.set_tag("model_name","Decision Tree")
+    mlflow.set_tag("model_name","Random Forest")
